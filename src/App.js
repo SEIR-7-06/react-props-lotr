eeea935a02826e78fbaa2baa1f1b167b470692a0
cotsao <codytsao@gmail.com>
2021-08-19 11:37:00 -0700

    HW complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './components/Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    let movieObj = [
+      {
+      title: "The Fellowship of the Ring",
+      hours: 2,
+      minutes:58
+      },
+      {
+        title: 'The Two Towers',
+        hours: 2,
+        minutes:59
+      },
+      {
+        title: "The Return of the King",
+        hours: 3,
+        minutes:21
+        }
+  ]
+    const movies =[]
+    for (let i = 0; i <movieObj.length; i++){
+      movies.push(<Movie 
+        title={movieObj[i].title} 
+        hours={movieObj[i].hours} 
+        minutes={movieObj[i].minutes}/>)
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <div className="allmovies">
+            {movies}
+        </div>
+        </header>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
