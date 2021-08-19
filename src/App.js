95a3868d24575b7564196de424b85f58664e8e75
gregdanko92 <gregdanko92@gmail.com>
2021-08-19 19:50:52 -0400

    re-finished hw

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Movies from './components/Movies'
 
+const movieData = [
+  {title: 'The Fellowship of the Ring',
+  runtime: '2h 58min'},
+  {title: 'The Two Towers',
+  runtime: '3h 21min'},
+  {title: 'The Return of the King',
+  runtime: '2h 59min'}
+]
 
 
 function App() {
   
   return (
     <div className="App">
-      <Movies />
+      <Movies movieData={movieData}/>
     </div>
   );
 }
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,18 +1,26 @@
-function Movies(props){
-const prefix = 'The Lord of the Rings: '
-const movieNames = ['The Fellowship of the Ring', 'The Two Towers', 'The Return of the King']
-const runTimes = ['2h 58min','3h 21min','2h 59min']
-for (let i=0;i<movieNames.length;i++){
+import React from 'react'
 
-    return(
-        <div>
-            <h2>{prefix + movieNames[i]}</h2>
-            <p>{runTimes[i]}</p>
-            <h2>{prefix + movieNames[i+1]}</h2>
-            <p>{runTimes[i+1]}</p>
-            <h2>{prefix + movieNames[i+2]}</h2>
-            <p>{runTimes[i+2]}</p>
-        </div>)
-    }
+function Movies(props){
+
+
+const prefix = 'The Lord of the Rings: '
+const movieJSX = []
+
+for(let i=0;i<props.movieData.length;i++){
+    movieJSX.push(
+      <>
+          <h2>{prefix + props.movieData[i].title}</h2>
+          <p>{props.movieData[i].runtime}</p>
+      </>  
+    )
 }
+return (
+    <>
+        {movieJSX}
+    </>
+)
+
+}
+ 
+  
 export default Movies;
