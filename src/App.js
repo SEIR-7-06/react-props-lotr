c3361b1abed65271b326b93ad5d8a719e4f61aed
jekkiboi <hollymayer4@gmail.com>
2021-08-20 14:47:04 -0700

    my work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Movies from './components/Movies'
+
 
 function App() {
+
+  const moviesArray = [
+    {
+      movieName: 'The Fellowship of the Ring',
+      movieLength: '2hrs 58min'
+    },
+    {
+      movieName: 'The Two Towers',
+      movieLength: '2hrs 59min'
+    },
+    {
+      movieName: 'The Return of the King',
+      movieLength: '3hrs 21min'
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Movies moviesArray={moviesArray} />
     </div>
   );
 }
