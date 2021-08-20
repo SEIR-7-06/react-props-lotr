5a4e89e944bb85a44c05d5f3383d1f5d590f6918
off2mxrs <off2mxrs@gmail.com>
2021-08-20 09:50:54 -0700

    Lotr For loop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import './App.css';
 import Movie from './components/Movie';
 
 function App() {
+  let title = [
+    {
+      lotr: "The Fellowship of the Ring",
+      hours: "2",
+      minutes:"58"
+
+    },
+    {
+      lotr:"The Two Towers",
+      hours: "2",
+      minutes:"59"
+
+    }, 
+    {
+      lotr: "The Return of the King",
+      hours: "3",
+      minutes:"21"
+    }, 
+
+
+      ]
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,9 +34,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      <Movie title="The Fellowship of the Ring" hours="2" minutes="58" />
-      <Movie title="The Two Towers" hours="2" minutes="59" />
-      <Movie title="The Return of the King" hours="3" minutes="21" />
+        <Movie title={title}/>
+    
+      {/* <Movie title="The Two Towers" hours="2" minutes="59" />
+      <Movie title="The Return of the King" hours="3" minutes="21" /> */}
       </header>
     </div>
   );
