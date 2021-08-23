189c285001228433edd69059296184a896c4106c
brookecalhoun <brookecalhouncoding@gmail.com>
2021-08-23 13:34:58 -0700

    finished hw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Movie from './components/Movie'
 
 function App() {
   return (
@@ -9,9 +10,30 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Movie 
+          movieName='The Fellowship of the Ring' 
+          movieLength='2h 58min' />
+          <Movie 
+          movieName='The Two Towers' 
+          movieLength='2h 59min' />
+          <Movie 
+          movieName='The Return of the King' 
+          movieLength='3h 21min' />
+        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
+
+
+
+
+
