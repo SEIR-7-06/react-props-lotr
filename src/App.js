082d04a42414a10093277906edf3971f8348c108
marshallwatrous <marshallwatrous95@gmail.com>
2021-08-24 15:16:25 -0700

    Adds completed assisgnment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Movie from "./components/Movie";
+
+import logo from './logo.svg';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="small">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Movie movieTitle="The Lord of the Rings: The Fellowship of the Ring" runTime="2h 58min" />
+        <Movie movieTitle="The Lord of the Rings: The Two Towers" runTime="2h 59min"/>
+        <Movie movieTitle="The Lord of the Rings: The Return of the King" runTime="3h 21min"/>
+        
       </header>
+      
     </div>
   );
 }
